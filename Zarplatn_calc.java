4cea7863669d38e918e5ea385c85f7c019b0bfbf
GigulevTolia <rich-tolik@mail.ru>
2013-03-09 01:21:35 +0600

    Update Zarplatn_calc.java

--- Zarplatn_calc.java
+++ Zarplatn_calc.java
@@ -1,3 +1,28 @@
+import java.io.*;
+
+/* Выполнение всех приложений Java начинается с вызова метода main () (точка начала выполнения программы).
+Ключевое слово p u b l i c — модификатор доступа, который позволяет управлять
+видимостью членов класса. Ключевое слово static позволяет вызывать метод
+main () без конкретизации экземпляра класса. Часть String args [ ] объявляет
+параметр args, который представляет собой массив экземпляров класса String.*/
+
+class zp {
+//Строка IOException необходима для обработки ошибок ввода
+public static void main(String args[]) throws IOException {
+double krm,kod,oklad,esn,ndfl,pf,fssbezrab,fssneschsl,otraboklad,summanalogov,itogo;
+BufferedReader kb=new BufferedReader(new InputStreamReader(System.in));
+
+System.out.println("vvedite kolichestvo rabochih dnei v mesyace");
+krm=Double.parseDouble(kb.readLine());
+
+System.out.println("vvedite kolichestvo otrabotannih dney");
+kod=Double.parseDouble(kb.readLine());
+
+System.out.println("vvedite oklad");
+oklad=Double.parseDouble(kb.readLine());
+esn=((oklad/krm)*kod)*0.036;
+esn=Math.round(esn);
+
 System.out.println("ESN sostavit (rub):"+esn);
 ndfl=((oklad/krm)*kod)*0.13;
 ndfl=Math.round(ndfl);
